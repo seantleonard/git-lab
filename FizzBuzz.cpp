3b3b2509d82e78fb71c4e79525035777e2e8432d
Christie Hennes <hennes@usc.edu>
2014-04-01 16:41:09 -0700

    REDO

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
  
 int main(int argc, char *argv[]) {
-	for(unsigned int i = 0; i < 100; i++) {
-		if( i % 3 == 0)
-			std::cout << i << ": " << "Fizz" << std::endl;
-	}
+	
 	
 	return(0);
 }
